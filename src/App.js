8d92b041ba5159c5e414ec6d224ba0d8bbb1f241
ppirunluck <140296826+ppirunluck@users.noreply.github.com>
2023-09-25 11:57:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import boxes from "./boxes"
+import Box from "./Box"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const [starWarsData, setStarWarsData] = React.useState({})
+  const [count, setCount] = React.useState(1)
+
+  // console.log("render");
+  // const [squares, setSquares] = React.useState(boxes)
+
+  // function toggle(id){
+  //   console.log(id);
+  //   setSquares(prevSquares => {
+  //     const newSquares = []
+  //     for(let i = 0; i < prevSquares.length; i++){
+  //       const currentSquare = prevSquares[i]
+  //       if(currentSquare.id === id){
+  //         const updatedSquare = {
+  //           ...currentSquare, 
+  //           on: !currentSquare.on
+  //         }
+  //         newSquares.push(updatedSquare)
+  //       }else{
+  //         newSquares.push(currentSquare)
+  //       }
+  //     }
+  //   return newSquares
+  //   })
+  // }
+
+  // function toggle(id){
+  //   setSquares(prevSquares => {
+  //     return prevSquares.map((square) => {
+  //       return square.id === id ? {...square, on: !square.on} : square
+  //     })
+  //   })
+  // }
+
+  // const squareElements = squares.map(square => (
+  //   <Box 
+  //     key={square.id}
+  //     on={square.on} 
+  //     toggle={()=>toggle(square.id)}
+  //   />
+  // ))
+
+  React.useEffect(function(){
+    console.log("Effect Ran");
+    fetch(`https://swapi.dev/api/people/${count}`)
+    .then(res => res.json())
+    .then(data => setStarWarsData(data))
+  },[count])
+
+  return(
+    <div>
+      <h2>This count is {count}</h2>
+      <button onClick={() => setCount(prevCount => prevCount+1)}>Get next character</button>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
-  );
+  )
 }
-
-export default App;
